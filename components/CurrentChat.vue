c2ea0139d4776caf7fe1911fe336687d05fa289b
Danish <danish.danish.danish2016@gmail.com>
2025-02-09 22:32:19 +0500

    add chat message components and update CurrentChat layout

--- components/CurrentChat.vue
+++ components/CurrentChat.vue
@@ -1,12 +1,18 @@
 <template>
-  <div id="currentChat" class="">
-    <div class="p-20 h-full bg-ui-background hover:bg-ui-hover">
-      <div class="text-center text-ui-foreground">UI</div>
-      <div
-        class="text-xl rounded text-center p-20 hover:bg-chat-hover bg-background text-foreground font-medium"
-      >
-        Hello
+  <div id="currentChat" class="flex flex-col">
+    <div class="py-2 px-3 text-ui-foreground bg-ui-background flex">
+      <div class="flex">
+        <img class="rounded-full" src="https://placehold.co/40x40" alt="" />
       </div>
+    </div>
+    <div class="bg-chat-background overflow-y-auto  py-1 px-3 h-full">
+      <Message v-for="n in 20" />
+      <Message v-for="n in 20" :isSent="true" />
+    </div>
+
+    <!-- New Message -->
+    <div class="flex bg-ui-background px-3 py-2">
+      <input type="text" class="py-2 px-2 w-full bg-ui-focus rounded-2xl" />
     </div>
   </div>
 </template>
---
+++ components/Message.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="flex mb-1 text-foreground"
+    :class="
+      isSent ? 'sent-message justify-end' : 'justify-start received-message'
+    "
+  >
+    <div
+      class="px-3 py-2 rounded"
+      :class="isSent ? 'bg-chat-sent' : 'bg-chat-recieved'"
+    >
+      {{ content }}
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  isSent: {
+    type: Boolean,
+    default: false,
+  },
+  content: {
+    type: String,
+    default: "Default Message",
+  },
+});
+</script>
+<style lang="postcss" scoped>
+.message {
+  /* @apply flex text-ui-foreground; */
+
+  /* > div {
+    @apply;
+  } */
+}
+.sent-message {
+  /* styles for sent message */
+  /* @apply justify-end;
+
+  > div {
+    @apply bg-chat-sent;
+  } */
+}
+
+.received-message {
+  /* styles for received message */
+  /* @apply justify-start;
+
+  > div {
+    @apply bg-chat-recieved;
+  } */
+}
+</style>
